4cfe65fac05988e4e41dc13098b45e8f9792b6f8
charlietrypsin <kharlz.eldrid@gmail.com>
2017-07-27 12:35:18 +0100

    New line reader
    Have used zip/split to pull out the first part of each line, need to
    edit to get the second part now, split by variable number of tabs

--- parameters.py
+++ parameters.py
@@ -24,38 +24,47 @@ for dirpath, dirnames, filenames in walk(dataFn):
 		experiment_name = foldername.replace('.raw', '')
 		print(foldername)
 		with open(dirpath + "/" + "_extern.inf") as info:
-			writer_list = []
-			write = open(experiment_name + '_parameters.txt', 'w+')
-			writer = csv.writer(write, lineterminator = '\n')
 			for line in info:
-				if "Capillary (kV)" in line:
-					writer_list.append(line)
-				if "Sampling Cone" in line:
-					writer_list.append(line)
-				if "Source Temperature" in line:
-					writer_list.append(line)
-				if "Trap Collision Energy" in line:
-					writer_list.append(line)
-				if "Start Mass" in line:
-					writer_list.append(line)
-				if "MSMS End Mass" in line:
-					writer_list.append(line)
-				if "Trap DC Bias" in line:
-					writer_list.append(line)
-				if "Backing" in line:
-					writer_list.append(line)
-				if "IMS Wave Height" in line:
-					writer_list.append(line)
-				if "IMS Wave Velocity" in line:
-					writer_list.append(line)
-				if "Transfer Wave Height" in line:
-					writer_list.append(line)
-				if "Transfer Wave Velocity" in line:
-					writer_list.append(line)
-				if "LM Resolution" in line:
-					writer_list.append(line)
-				if "HM Resolution" in line:
-					writer_list.append(line)
+				writing_list = zip(*(line1.strip().split('\t') for line1 in info))
 
-			#print(writer_list)
-			writer.writerow(writer_list)
+
+for item in writing_list[0]:
+	print(item)
+	
+# print(writing_list[0])
+			# writer_list = []
+			# write = open(experiment_name + '_parameters.txt', 'w+')
+			# writer = csv.writer(write, lineterminator = '\n')
+			# for line in info:
+			# 	if "Capillary (kV)" in line:
+			# 		writer_list.append(line)
+			# 	if "Sampling Cone" in line:
+			# 		writer_list.append(line)
+			# 	if "Source Temperature" in line:
+			# 		writer_list.append(line)
+			# 	if "Trap Collision Energy" in line:
+			# 		writer_list.append(line)
+			# 	if "Start Mass" in line:
+			# 		writer_list.append(line)
+			# 	if "MSMS End Mass" in line:
+			# 		writer_list.append(line)
+			# 	if "Trap DC Bias" in line:
+			# 		writer_list.append(line)
+			# 	if "Backing" in line:
+			# 		writer_list.append(line)
+			# 	if "IMS Wave Height" in line:
+			# 		writer_list.append(line)
+			# 	if "IMS Wave Velocity" in line:
+			# 		writer_list.append(line)
+			# 	if "Transfer Wave Height" in line:
+			# 		writer_list.append(line)
+			# 	if "Transfer Wave Velocity" in line:
+			# 		writer_list.append(line)
+			# 	if "LM Resolution" in line:
+			# 		writer_list.append(line)
+			# 	if "HM Resolution" in line:
+			# 		writer_list.append(line)
+
+			# print(writer_list)
+			# writer.writerow(writer_list)
+
